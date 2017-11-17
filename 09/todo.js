f14d3fb1f4c41fb537507a9c9de0ece830333324
boffin123 <boffin123@naver.com>
2017-11-17 16:16:15 +0900

    09

---
+++ 09/todo.js
@@ -0,0 +1,19 @@
+var new_task = document.getElementsByClassName('new-task');
+var del = document.getElementsByClassName('delete');
+var text = document.getElementsByClassName('text');
+var chec = document.getElementsByClassName('toggle-checked');
+
+
+
+del.addEventListener('click', function(){
+
+
+
+});
+
+
+new_task.addEventListener('keyup', function(){
+    if(event.keycode)
+
+
+});
